6a82d704367d43a3d9bfb71a7a1527ae4b904040
blekkk <fazailman992@gmail.com>
2022-04-05 23:31:41 +0700

    added style to editor and refactor code structure

--- plugins/code-editor/admin/src/containers/Components/Editor/Editor.js
+++ plugins/code-editor/admin/src/containers/Components/Editor/Editor.js
@@ -1,31 +1,60 @@
-import React from 'react';
-import Editor from 'react-simple-code-editor';
-import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
- 
-const code = `function add(a, b) {
-  return a + b;
-}
-`;
- 
-class CodeEditor extends React.Component {
-  state = { code };
- 
-  render() {
-    return (
-      <Editor
-        value={this.state.code}
-        onValueChange={code => this.setState({ code })}
-        highlight={code => highlight(code, languages.js)}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
-        }}
-      />
-    );
+import React, { useRef, useEffect } from 'react';
+import './Editor.css';
+
+const Editor = (props) => {
+  const lineCounterRef = useRef(null);
+  const codeEditorRef = useRef(null);
+  let lineCountCache;
+
+  useEffect(() => {
+    handleLineCount()
+  }, []);
+
+  const handleScroll = () => {
+    const lineCounter = lineCounterRef.current;
+    const codeEditor = codeEditorRef.current;
+
+    lineCounter.scrollTop = codeEditor.scrollTop;
+    lineCounter.scrollLeft = codeEditor.scrollLeft;
   }
+
+  const handleLineCount = () => {
+    const lineCounter = lineCounterRef.current;
+    const codeEditor = codeEditorRef.current;
+
+    let lineCount = codeEditor.value.split('\n').length;
+    let outarr = [];
+    if (lineCountCache != lineCount) {
+      for (let x = 0; x < lineCount; x++) {
+        outarr[x] = (x + 1) + '.';
+      }
+      lineCounter.value = outarr.join('\n');
+    }
+    lineCountCache = lineCount;
+  }
+
+  return (
+    <div style={{ display: 'flex', height: '100%' }}>
+      <textarea
+        id='line-counter'
+        wrap='off'
+        readonly='true'
+        ref={lineCounterRef}
+      >
+      </textarea>
+      <textarea
+        id='code-editor'
+        value={props.value}
+        placeholder='Enter Source code here'
+        ref={codeEditorRef}
+        wrap='off'
+        onChange={props.onChangeHandler}
+        onScroll={handleScroll}
+        onInput={handleLineCount}
+      >
+      </textarea>
+    </div>
+  );
 }
 
-export default CodeEditor;
+export default Editor;
--- plugins/code-editor/admin/src/containers/Pages/HomePage/index.js
+++ plugins/code-editor/admin/src/containers/Pages/HomePage/index.js
@@ -1,31 +1,33 @@
-/*
- *
- * HomePage
- *
- */
 import './HomePage.css';
 import React, { memo, useState } from 'react';
+import Editor from '../../Components/Editor/Editor';
 
 const HomePage = () => {
-  const [code, setCode] = useState(`// Enter source code here
-  <h1>
-    Hello, world!
-  </h1>
-  `)
+  const [code, setCode] = useState(
+`<! -- Enter source code here -->
+<h1>
+  Hello, world!
+</h1>
+`)
+
+  const handleChange = (ev) => {
+    setCode(ev.target.value);
+  }
 
   return (
     <div className='homepage-wrapper'>
-      <iframe srcDoc={code} style={{width: '60%', height: 'calc(100vh - 6rem)', overflowY: 'scroll'}}></iframe>
-      <div style={{width: '40%'}}>
+      <iframe srcDoc={code} style={{ width: '60%', height: 'calc(100vh - 6rem)', overflowY: 'scroll' }}></iframe>
+      <div style={{ width: '40%' }}>
         <div className='side-editor'>
           <div className='side-editor-content'>
-            <h1>Source Code:</h1>
-            <textarea
+            <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '8px'}}>
+              <h1>Source Code:</h1>
+              <button>Save</button>
+            </div>
+            <Editor 
               value={code}
-              placeholder='Enter Source code here'
-              onChange={(ev) => {setCode(ev.target.value)}}
-            >           
-            </textarea>
+              onChangeHandler={handleChange}
+            />
           </div>
         </div>
       </div>
